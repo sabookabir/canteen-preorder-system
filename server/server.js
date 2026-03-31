00477f53e08090af4d8e0ffc58fa32361a047b61
Sabookabir <Sabookabir@gmail.com>
2026-03-31 09:10:20 +0530

    🔧 Fix CORS origin

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.set('socketio', io);
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin: process.env.CLIENT_URL || 'http://localhost:5173',
   credentials: true,
 }));
 app.use(express.json());
